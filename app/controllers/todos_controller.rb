6951083ba86105deff5a38ab63349ad4cd9cade2
Apprentice <apprentice@dbc24.local>
2013-07-01 19:44:09 -0700

    initial commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,45 @@
+class TodosController < ApplicationController
+  before_filter :load_todos
+
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def show
+    @todo = Todo.find params[:id]
+    @count = Todo.where(list_name: @todo.list_name).count
+  end
+
+  def create
+    params[:todo][:list_name] = params[:todo][:list_name].parameterize
+    @todo = Todo.new(params[:todo])
+    if @todo.save
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @todo = Todo.find params[:id]
+  end
+
+  def update
+    @todo = Todo.find params[:id]
+    if @todo.update_attributes params[:todo]
+      redirect_to @todo
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def load_todos
+    @todos = Todo.all
+  end
+end
